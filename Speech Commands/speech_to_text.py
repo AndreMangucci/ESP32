82b91571054f2ea96916c07517d7c72746f612d1
André Hermann <andremangucci@gmail.com>
2018-10-16 20:04:57 -0300

    Add files via upload

--- Speech Commands/speech_to_text.py
+++ Speech Commands/speech_to_text.py
@@ -2,9 +2,10 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 with sr.Microphone() as source:
+    r.adjust_for_ambient_noise(source)
     while 1 :
         print("Speak Anything :")
-        audio = r.listen(source)
+        audio = r.listen(source,timeout=10.0)
         try:
             text = r.recognize_google(audio)
             print("You said : {}".format(text))
